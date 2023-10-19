28c602da25771dda9825397eeaf94e93d2ef28d2
David Marchante <davidmarchan@gmail.com>
2023-10-19 03:10:01 -0400

    feat: Execute lua file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use mlua::Lua;
+
+fn main() -> Result<()> {
+    let code = std::fs::read_to_string("test.lua").unwrap();
+
+    let lua = Lua::new();
+    lua.load(code).exec()?;
+
+    Ok(())
 }
