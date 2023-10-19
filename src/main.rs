caea994bbc980dabfbd84ff23ad1203cdc475327
David Marchante <davidmarchan@gmail.com>
2023-10-19 03:27:10 -0400

    feat: Mutating globals from lua and reading them back in Rust

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,16 @@ fn main() -> Result<()> {
 
     let lua = Lua::new();
     let globals = lua.globals();
+    let non_magical_number = 42;
 
     globals.set("message_from_rust", "HEDDO? (in Rust)")?;
+    globals.set("non_magical_number", non_magical_number)?;
 
     lua.load(code).exec()?;
+
+    let non_magical_number: i32 = globals.get("non_magical_number")?;
+
+    eprintln!("non_magical_number = {non_magical_number:?}");
 
     Ok(())
 }
--- test.lua
+++ test.lua
@@ -1,2 +1,11 @@
+---@type string
+message_from_rust = message_from_rust
+---@type number
+non_magical_number = non_magical_number
+
 print("HEDDO! (from Lua)")
 print(message_from_rust)
+print("non_magical_number = " .. non_magical_number)
+
+non_magical_number = non_magical_number * 2
+print("non_magical_number = " .. non_magical_number)
